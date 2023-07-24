ca2e79fe75cf6236fbb1954147735d4f7f2074b9
dustinwalker <me@dustinwalker.com>
2023-07-24 15:09:41 -0500

    ADD: Major updates, need to test

--- src/services/VideoEmbedService.php
+++ src/services/VideoEmbedService.php
@@ -16,83 +16,73 @@ use GuzzleHttp\Exception\GuzzleException;
  * @package astuteo\astuteotoolkit\services
  */
 class VideoEmbedService extends Component {
-    public static function getEmbedInfo($url) {
-        if(AstuteoToolkit::$plugin->getSettings()->cacheVideoEmbeds && Craft::$app->cache->exists($url)) {
+    public static function getEmbedInfo($url): ?array {
+        $settings = AstuteoToolkit::$plugin->getSettings();
+        if($settings->cacheVideoEmbeds && Craft::$app->cache->exists($url)) {
             return Craft::$app->cache->get($url);
         }
-        $embedInfo = [
-            'id' => '',
-            'url' => '',
-            'thumbnail' => '',
-            'staticThumb' => true,
-            'provider' => 'unknown'
-        ];
-        if(self::isYouTube($url)) {
-            $videoId = self::getYouTubeId($url);
-            if(AstuteoToolkit::$plugin->getSettings()->uploadVideoThumbs && AstuteoToolkit::$plugin->getSettings()->uploadVideoThumbsVolumeId) {
-                $volumeId = AstuteoToolkit::$plugin->getSettings()->uploadVideoThumbsVolumeId;
-                $thumbLocal = self::storeThumbnail(self::createYouTubeThumbnailMax($videoId), $videoId, $volumeId);
-                if($thumbLocal === false) {
-                    $thumb = [
-                        'url' => self::createYouTubeThumbnailMax($videoId)
-                    ];
-                    $staticThumb = true;
-                } else {
-                    $thumb = self::getThumbAsset($videoId . '.jpg', $volumeId);
-                    $staticThumb = false;
-                }
-            } else {
-                $thumb = [
-                    'url' => self::createYouTubeThumbnailMax($videoId)
-                ];
-                $staticThumb = true;
-            }
-            $embedInfo = [
-                'id' => $videoId,
-                'url' => self::createYouTubeEmbed($videoId),
-                'thumbnail' => $thumb,
-                'staticThumb' => $staticThumb,
-                'provider' => 'youtube'
-            ];
 
-        } elseif(self::isVimeo($url)) {
-            $videoId = self::getVimeoId($url);
-            $embedInfo = [
-                'id' => $videoId,
-                'url' => self::createVimeoEmbed($videoId),
-                'thumbnail' => [
-                    'url' => self::createVimeoThumbnail($videoId),
-                ],
+        return match (true) {
+            self::isYouTube($url) => self::getYouTubeEmbedInfo($url, $settings),
+            self::isVimeo($url) => self::getVimeoEmbedInfo($url, $settings),
+            default => [
+                'id' => '',
+                'url' => '',
+                'thumbnail' => '',
                 'staticThumb' => true,
-                'provider' => 'vimeo'
-            ];
+                'provider' => 'unknown',
+            ],
+        };
+    }
+    private static function getYouTubeEmbedInfo(string $url, $settings): ?array {
+        $videoId = self::getYouTubeId($url);
+        $embedInfo = [
+            'id' => $videoId,
+            'url' => self::createYouTubeEmbed($videoId),
+            'provider' => 'youtube',
+        ];
+
+        if($settings->uploadVideoThumbs && $settings->uploadVideoThumbsVolumeId) {
+            $volumeId = $settings->uploadVideoThumbsVolumeId;
+            $thumbLocal = self::storeThumbnail(self::createYouTubeThumbnailMax($videoId), $videoId, $volumeId);
+
+            $embedInfo['thumbnail'] = $thumbLocal !== false
+                ? self::getThumbAsset($videoId . '.jpg', $volumeId)
+                : ['url' => self::createYouTubeThumbnailMax($videoId)];
+
+            $embedInfo['staticThumb'] = $thumbLocal === false;
+        } else {
+            $embedInfo['thumbnail'] = ['url' => self::createYouTubeThumbnailMax($videoId)];
+            $embedInfo['staticThumb'] = true;
         }
-        /*
-         * Check on various states of caching
-         * If we are downloading thumbs, check to make sure all the conditions
-         * are met. If we successfully downloaded the thumb, cache the results
-         * but don't if we want to download them and it hasn't yet grabbed it from
-         * YouTube
-         *
-         * Note: There is likely a much better way for the conditions.
-         */
-        if(
-            AstuteoToolkit::$plugin->getSettings()->cacheVideoEmbeds
-            && isset($videoId)
-            && isset($volumeId)
-            && AstuteoToolkit::$plugin->getSettings()->uploadVideoThumbs
-            && AstuteoToolkit::$plugin->getSettings()->uploadVideoThumbsVolumeId)
-        {
-            {
-                if(self::getThumbAsset($videoId . 'jpg', $volumeId)) {
-                    Craft::$app->cache->set($url, $embedInfo);
-                }
+
+        if($settings->cacheVideoEmbeds && isset($videoId, $volumeId) && $settings->uploadVideoThumbs && $settings->uploadVideoThumbsVolumeId) {
+            if(self::getThumbAsset($videoId . 'jpg', $volumeId)) {
+                Craft::$app->cache->set($url, $embedInfo);
             }
-        }
-        elseif(AstuteoToolkit::$plugin->getSettings()->cacheVideoEmbeds)
-        {
+        } elseif($settings->cacheVideoEmbeds) {
             Craft::$app->cache->set($url, $embedInfo);
         }
+
+        return $embedInfo;
+    }
+
+    private static function getVimeoEmbedInfo(string $url, $settings): ?array {
+        $videoId = self::getVimeoId($url);
+        $embedInfo = [
+            'id' => $videoId,
+            'url' => self::createVimeoEmbed($videoId),
+            'thumbnail' => [
+                'url' => self::createVimeoThumbnail($videoId),
+            ],
+            'staticThumb' => true,
+            'provider' => 'vimeo',
+        ];
+
+        if($settings->cacheVideoEmbeds) {
+            Craft::$app->cache->set($url, $embedInfo);
+        }
+
         return $embedInfo;
     }
 
@@ -117,7 +107,8 @@ class VideoEmbedService extends Component {
         if($thumbnail) return $thumbnail;
     }
 
-    private static function thumbnailExists($url) {
+    private static function thumbnailExists($url): bool | string
+    {
         try {
             $response = Craft::$app->api->client->get($url);
             $code = $response->getStatusCode();
@@ -155,7 +146,8 @@ class VideoEmbedService extends Component {
         }
     }
 
-    private static function getThumbAsset($filename, $volumeId) {
+    private static function getThumbAsset($filename, $volumeId): bool|Asset
+    {
         $folderId = Craft::$app->assets->getRootFolderByVolumeId($volumeId)['id'];
         $asset =  Asset::findOne(['folderId' => $folderId, 'filename' => $filename]);
         if($asset) {
@@ -164,7 +156,8 @@ class VideoEmbedService extends Component {
         return false;
     }
 
-    private static function storeThumbnail($url, $id, $volumeId) {
+    private static function storeThumbnail($url, $id, $volumeId): bool|Asset
+    {
         $filename = $id . '.jpg';
         $existing = self::getThumbAsset($filename, $volumeId);
         if(is_object($existing)) {
@@ -183,43 +176,86 @@ class VideoEmbedService extends Component {
      * @param string $url
      * @return boolean
      */
-    public static function isYouTube($url)
+    public static function isYouTube(string $url): bool
     {
-        return (strpos($url, 'youtube.com/') !== false || strpos($url, 'youtu.be/') !== false);
+        return (str_contains($url, 'youtube.com/') || str_contains($url, 'youtu.be/') || str_contains($url, 'youtube-nocookie.com/'));
     }
     /**
      * Is the url a vimeo url
      * @param string $url
      * @return boolean
      */
-    public static function isVimeo($url)
+    public static function isVimeo(string $url): bool
     {
-        return strpos($url, 'vimeo.com/') !== FALSE;
+        return str_contains($url, 'vimeo.com/');
     }
 
     /**
      * Parse the YouTube URL, return the video ID
-     * https://gist.github.com/ghalusa/6c7f3a00fd2383e5ef33
+     * Covers these URL formats:
+     * - youtube.com/v/abc123
+     * - youtube.com/v/abc123?version=3&autohide=1
+     * - youtube.com/embed/abc123
+     * - youtube.com/embed/abc123?rel=0
+     * - youtube.com/watch?v=abc123
+     * - youtube.com/watch?v=abc123&feature=related
+     * - youtube.com/watch?v=abc123&playnext=1&list=abc123&feature=results_main
+     * - youtube.com/watch?v=abc123&feature=youtu.be
+     * - youtube.com/watch?feature=endscreen&v=abc123&NR=1
+     * - youtube.com/watch?feature=player_embedded&v=abc123
+     * - youtube.com/watch?feature=player_detailpage&v=abc123#t=31s
+     * - youtu.be/abc123
+     * - youtu.be/abc123?feature=youtu.be
+     * - youtube.com/user/abc123#p/u/1/abc123
+     * - youtube.com/user/abc123?feature=watch
+     * - youtube.com/user/abc123?v=abc123
+     * - youtube.com/user/abc123?v=abc123&feature=related
+     * - youtube-nocookie.com/v/abc123?version=3&autohide=1
+     * - youtube.com/c/ChannelName/live
      * @param string $url
-     * @return string
+     * @return bool|string
      */
-    public static function getYouTubeId($url)
+    public static function getYouTubeId(string $url): bool|string
     {
-        preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
-        return $match[1];
-    }
-    /**
-     * Parse the Vimeo URL, return the video ID
-     * @param string $url
-     * @return string
-     */
-    public static function getVimeoId($url)
-    {
-        preg_match('%^https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)(?:[?]?.*)$%im', $url, $matches);
-        if(!array_key_exists(3, $matches)) {
-            return false;
+        preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/|youtube.com/c/.*?/live)([^"&?/ ]{11})%i', $url, $match);
+        if (isset($match[1])) {
+            return $match[1];
         }
-        return $matches[3];
+        preg_match('/youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/', $url, $matchesLive);
+        if (isset($matchesLive[1])) {
+            return $matchesLive[1];
+        }
+        return false;
     }
 
+    /**
+     * Parse the Vimeo URL, return the video ID
+     * Covers these URL formats:
+     * - vimeo.com/abc123
+     * - vimeo.com/abc123?foo=bar
+     * - vimeo.com/channels/abc123
+     * - vimeo.com/channels/abc123/abc123
+     * - vimeo.com/groups/abc123/videos/abc123
+     * - vimeo.com/album/abc123/video/abc123
+     * - player.vimeo.com/video/abc123
+     * - vimeo.com/video/abc123
+     * - vimeo.com/video/abc123?foo=bar
+     * @param string $url
+     * @return bool|string
+     */
+    public static function getVimeoId(string $url): bool|string
+    {
+        preg_match('%^https?://(?:www\.|player\.)?vimeo.com/(?:channels/(?:\w+/)?|groups/([^/]*)/videos/|album/(\d+)/video/|video/|)(\d+)(?:$|/|\?)(?:[?]?.*)$%im', $url, $matches);
+        if (isset($matches[3])) {
+            return $matches[3];
+        }
+
+        // A simpler fallback to handle any other possible Vimeo URL formats.
+        preg_match('/vimeo.*\/(\d+)/i', $url, $fallbackMatches);
+        if (isset($fallbackMatches[1])) {
+            return $fallbackMatches[1];
+        }
+
+        return false;
+    }
 }
