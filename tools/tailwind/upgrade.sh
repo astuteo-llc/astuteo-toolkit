9bb69da3e9e775ca954e29d98de96a011ff73246
dustinwalker <me@dustinwalker.com>
2023-08-01 13:19:56 -0500

    ADD: Scope src files to css, sass, scss and pcss

--- tools/tailwind/upgrade.sh
+++ tools/tailwind/upgrade.sh
@@ -11,6 +11,9 @@ class_file=$1
 # List of directories to scan
 dirs="./src ./templates"
 
+# File types to scan in the src directory
+types="css sass scss pcss"
+
 while read -r line
 do
     old_class=$(echo $line | cut -d ' ' -f 1)
@@ -23,14 +26,30 @@ do
         if [ -d "$dir" ]
         then
             echo "Processing $dir"
-            # Find all files in the directory
-            files=$(grep -rl "$old_class" $dir)
-            for file in $files
-            do
-                echo "Updating $file"
-                # Use sed to replace old_class with new_class
-                sed -i "s/$old_class/$new_class/g" "$file"
-            done
+            # Differentiate between ./src and ./templates
+            if [ "$dir" == "./src" ]
+            then
+                for type in $types
+                do
+                    echo "Looking for $type files"
+                    # Find all files of the specific type in the directory
+                    files=$(find $dir -name "*.$type" -print0 | xargs -0 grep -l "$old_class")
+                    for file in $files
+                    do
+                        echo "Updating $file"
+                        # Use sed to replace old_class with new_class
+                        sed -i "s/$old_class/$new_class/g" "$file"
+                    done
+                done
+            else
+                files=$(grep -rl "$old_class" $dir)
+                for file in $files
+                do
+                    echo "Updating $file"
+                    # Use sed to replace old_class with new_class
+                    sed -i "s/$old_class/$new_class/g" "$file"
+                done
+            fi
         else
             echo "$dir doesn't exist"
         fi
